41de563ba245732c67a3111e17b6b9958eba7348
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-08-29 15:09:47 -0300

    Páginas do novo index_agendamento.php

---
+++ navibar_agendamento.php
@@ -0,0 +1,24 @@
+<div id="content" class="p-10 p-md-2">
+
+        <nav style="padding: 20px;"class="navbar-expand-lg navbar-light bg-light">
+        
+          <div class="container-fluid">
+
+            <button style="background-color: #66a7ff;" class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i style="border:none;"class="fa fa-bars"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+              <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item">
+                <form method = "POST" action = "pesquisa.php">
+                       
+                            
+                        </form> 
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <script src="mascara.js"></script>
