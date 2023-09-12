7bb21c9cd3cb1d3db3856048077cce503387b85e
jls2pb <j.lucas.1462@gmail.com>
2023-09-12 14:10:37 -0300

    ATULIZAÇÕES NO AGENDAMENTO

--- agendar/cadastrar_agendamento.php
+++ agendar/cadastrar_agendamento.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['cpf']) == FALSE) {
     header("Location:../index.php");
 }
 $cpf_logado = $_SESSION['cpf'];
-$nome = $_SESSION['id'];
+
 $id = $_GET['id'];
 ?>
 
@@ -36,74 +36,35 @@ include "../footer.php";
     <input type="hidden" name="cpf" id="cpf">
     <input type="hidden" name="cod" id="cod">
     <div class="col-4">
-        <div class="form-outline mb-4">
-            <label class="form-label">DIA DO ATENDIMENTO</label>
-            <select class="form-control form-control-lg" name="dia" id="dia">
-                <?php
-                // Conexão com o banco de dados
-                include "../conexao.php";
-                $sql = "select * from agenda_profissional where id_profissional = '$id'";
-                $resultado = $conexao->prepare($sql);
-                if ($resultado->execute()) {
-                    $x = $resultado->fetchAll();
-                }
+    <div class="form-outline mb-4">
+        <label class="form-label">DIA DO ATENDIMENTO</label>
+        <select class="form-control form-control-lg" name="dia" id="dia" onchange="carregarHorarios()">
+            <?php
+            // Conexão com o banco de dados
+            include "../conexao.php";
+            $sql = "select * from agenda_profissional where id_profissional = '$id'";
+            $resultado = $conexao->prepare($sql);
+            if ($resultado->execute()) {
+                $x = $resultado->fetchAll();
                 foreach ($x as $dado) {
                     ?>
                     <option value="<?php echo $dado['dia']; ?>"><?php echo $dado["dia"]; ?></option>
-                <?php }
-
-                ?>
-            </select>
-        </div>
+                    <?php
+                }
+            }
+            ?>
+        </select>
     </div>
-    <div class="col-4">
-        <div class="form-outline mb-4">
-            <label class="form-label">HORARIO DO ATENDIMENTO</label>
-            <select class="form-control form-control-lg" name="horario" required>
-                <?php
-                // Conexão com o banco de dados
-                $sql = "select * from agenda_profissional where id_profissional = '$id'";
-                $resultado = $conexao->prepare($sql);
-                if ($resultado->execute()) {
-                    $x = $resultado->fetchAll();
-                }
-                foreach ($x as $dado) {
-                    // Horário de início e final da manhã
-                    $inicioManha = strtotime($dado['inicio_manha']);
-                    $finalManha = strtotime($dado['final_manha']);
-
-                    // Horário de início e final da tarde
-                    $inicioTarde = strtotime($dado['inicio_tarde']);
-                    $finalTarde = strtotime($dado['final_tarde']);
-                }
-                $sql = "select * from profissionais where id_profissional = '$id'";
-                $resultado = $conexao->prepare($sql);
-                if ($resultado->execute()) {
-                    $x = $resultado->fetchAll();
-                }
-                foreach ($x as $dado) {
-                    $tempoAtendimento = $dado['tempo_atendimento'];
-                }
-
-                // Consulta ao banco para obter os horários disponíveis
-                $dia = $_POST['dia']; // Dia selecionado pelo usuário
-
-                // Loop para gerar os horários disponíveis com base no tempo de atendimento
-                for ($horario = $inicioManha; $horario <= $finalManha; $horario += 60 * $tempoAtendimento) {
-                    $horarioFormatado = date('H:i', $horario);
-                    echo "<option value='$horarioFormatado'>$horarioFormatado</option>";
-                }
-
-                for ($horario = $inicioTarde; $horario <= $finalTarde; $horario += 60 * $tempoAtendimento) {
-                    $horarioFormatado = date('H:i', $horario);
-                    echo "<option value='$horarioFormatado'>$horarioFormatado</option>";
-                }
-
-                // Feche a conexão com o banco de dados
-                ?>
-            </select><br><br>
-        </div>
+</div>
+<div class="col-4">
+    <div class="form-outline mb-4">
+        <label class="form-label">HORARIO DO ATENDIMENTO</label>
+        <select class="form-control form-control-lg" name="horario" id="horario" required>
+            <!-- Opções de horário serão carregadas dinamicamente aqui -->
+        </select><br><br>
     </div>
+</div>
+                
 </form>
 
 <script src="../mascara.js"></script>
@@ -180,4 +141,35 @@ include "../footer.php";
             });
         }
     });
+</script>
+<script>
+    function carregarHorarios() {
+    var diaSelecionado = document.getElementById("dia").value;
+    var horarioSelect = document.getElementById("horario");
+
+    // Limpar a lista de horários
+    horarioSelect.innerHTML = "<option value=''>Selecione um horário</option>";
+
+    // Realizar uma solicitação AJAX para obter os horários disponíveis
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "obter_horarios.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var horariosDisponiveis = JSON.parse(xhr.responseText);
+
+            // Preencher a lista de horários com base na resposta do servidor
+            for (var i = 0; i < horariosDisponiveis.length; i++) {
+                var option = document.createElement('option');
+                option.value = horariosDisponiveis[i];
+                option.text = horariosDisponiveis[i];
+                horarioSelect.appendChild(option);
+            }
+        }
+    };
+
+    xhr.send("dia=" + diaSelecionado);
+}
+
 </script>
---
+++ agendar/obter_horarios.php
@@ -0,0 +1,54 @@
+<?php
+// Verifique se a solicitação é um POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Conexão com o banco de dados
+    include "../conexao.php";
+
+    // Obter o dia selecionado pelo usuário da solicitação POST
+    $diaSelecionado = $_POST['dia'];
+    $id = 1;
+    // Consulta ao banco de dados para obter os horários disponíveis com base no dia selecionado
+    $sql = "SELECT * FROM agenda_profissional WHERE id_profissional = :id_profissional AND dia = :dia";
+    $resultado = $conexao->prepare($sql);
+    $resultado->bindParam(':id_profissional', $id, PDO::PARAM_INT);
+    $resultado->bindParam(':dia', $diaSelecionado, PDO::PARAM_STR);
+
+    // Inicializar um array para armazenar os horários disponíveis
+    $horariosDisponiveis = [];
+
+    if ($resultado->execute()) {
+        $x = $resultado->fetchAll();
+        foreach ($x as $dado) {
+            // Horário de início e final da manhã
+            $inicioManha = strtotime($dado['inicio_manha']);
+            $finalManha = strtotime($dado['final_manha']);
+
+            // Horário de início e final da tarde
+            $inicioTarde = strtotime($dado['inicio_tarde']);
+            $finalTarde = strtotime($dado['final_tarde']);
+
+            // Tempo de atendimento
+            $tempoAtendimento = $dado['tempo_atendimento'];
+
+            // Gerar horários disponíveis e adicioná-los ao array PHP
+            for ($horario = $inicioManha; $horario <= $finalManha; $horario += 60 * $tempoAtendimento) {
+                $horariosDisponiveis[] = date('H:i', $horario);
+            }
+
+            for ($horario = $inicioTarde; $horario <= $finalTarde; $horario += 60 * $tempoAtendimento) {
+                $horariosDisponiveis[] = date('H:i', $horario);
+            }
+        }
+    }
+
+    // Defina o cabeçalho Content-Type como JSON
+    header('Content-Type: application/json');
+
+    // Retornar os horários disponíveis como um JSON
+    echo json_encode($horariosDisponiveis);
+} else {
+    // Responda com um erro se a solicitação não for um POST
+    http_response_code(400); // Bad Request
+    echo json_encode(["error" => "Solicitação inválida"]);
+}
+?>
