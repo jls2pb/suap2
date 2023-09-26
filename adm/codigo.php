bcb3a80ab5f9d5c62704c59998e809ad393f50cb
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-09-26 08:28:00 -0300

    código alfanumerico

--- adm/codigo.php
+++ adm/codigo.php
@@ -11,8 +11,20 @@ if (isset($_SESSION['cpf_adm'])) {
 
 <?php
 // Função para gerar um número aleatório de 8 a 10 dígitos
-function gerarNumeroAleatorio() {
-    return rand(10000000, 10000000000);
+function gerarNumeroAleatorio($minLength = 20, $maxLength = 22) {
+    $salt = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $len = strlen($salt);
+    $pass = '';
+
+    mt_srand(10000000*(double)microtime());
+    
+    $length = mt_rand($minLength, $maxLength); // Gere um comprimento aleatório dentro do intervalo especificado
+
+    for ($i = 0; $i < $length; $i++) {
+        $pass .= $salt[mt_rand(0, $len - 1)];
+    }
+    
+    return $pass;
 }
 
 if (isset($_SESSION['data_geracao'])) {
@@ -25,7 +37,7 @@ if (isset($_SESSION['data_geracao'])) {
 
         // Usar prepared statement para evitar SQL Injection
         $stmt = $conexao->prepare("INSERT INTO codigo (numero, data_geracao) VALUES (?, NOW())");
-        $stmt->bindParam(1, $numeroAleatorio, PDO::PARAM_INT);
+        $stmt->bindParam(1, $numeroAleatorio, PDO::PARAM_STR);
         $stmt->execute();
     } else {
         $numeroAleatorio = $_SESSION['numero'];
@@ -39,7 +51,7 @@ if (isset($_SESSION['data_geracao'])) {
 
     // Usar prepared statement para evitar SQL Injection
     $stmt = $conexao->prepare("INSERT INTO codigo (numero, data_geracao) VALUES (?, NOW())");
-    $stmt->bindParam(1, $numeroAleatorio, PDO::PARAM_INT);
+    $stmt->bindParam(1, $numeroAleatorio, PDO::PARAM_STR);
     $stmt->execute();
 }
 ?>
