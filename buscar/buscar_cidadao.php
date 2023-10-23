6aa9d2f742ec61af24df302c0b343b450344aa43
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-10-23 13:59:06 -0300

    Update buscar_cidadao.php

--- buscar/buscar_cidadao.php
+++ buscar/buscar_cidadao.php
@@ -12,7 +12,7 @@ if (isset($_GET['term'])) {
     // Verifique se o termo de pesquisa possui pelo menos 3 letras
     if (strlen($term) >= 3) {
         // Consulta para obter os procedimentos que correspondem ao termo de pesquisa
-        $sql = "SELECT DISTINCT no_cidadao, nu_cpf, nu_cns, dt_nascimento, no_mae, nu_telefone_celular, nu_telefone_contato  FROM ta_cidadao WHERE no_cidadao LIKE :term LIMIT 5";
+        $sql = "SELECT DISTINCT ON (no_cidadao, nu_cpf, nu_cns) no_cidadao, nu_cpf, nu_cns, dt_nascimento, no_mae, nu_telefone_celular, nu_telefone_contato  FROM ta_cidadao WHERE no_cidadao LIKE :term LIMIT 5";
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':term', '%' . $term . '%', PDO::PARAM_STR);
         $stmt->execute();
