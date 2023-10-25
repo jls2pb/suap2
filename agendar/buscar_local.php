8f2bbdf61de63e4e5fa4989c1790ae95bc092f3d
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-10-25 10:52:26 -0300

    Create buscar_local.php

---
+++ agendar/buscar_local.php
@@ -0,0 +1,25 @@
+<?php
+   $host = "localhost";
+   $port = 5432;
+   $database = "suap";
+   $user = "postgres";
+   $password = "1234";
+   $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$database;user=$user;password=$password");
+
+if (isset($_GET['term'])) {
+    $term = $_GET['term'];
+
+    // Verifique se o termo de pesquisa possui pelo menos 3 letras
+    if (strlen($term) >= 3) {
+        // Consulta para obter os procedimentos que correspondem ao termo de pesquisa
+        $sql = "SELECT nome_fantasia FROM local_atendimento WHERE nome_fantasia LIKE :term";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':term', '%' . $term . '%', PDO::PARAM_STR);
+        $stmt->execute();
+
+        // Retorne os procedimentos encontrados em um array JSON
+        $nome_fantasia = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        echo json_encode($nome_fantasia);
+    }
+}
+?>
