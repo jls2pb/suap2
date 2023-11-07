1c35d55bfc67d8277584e604d6fae21c91539034
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-11-07 09:19:02 -0300

    ajustando data modificação

--- adm/registro_agendamento.php
+++ adm/registro_agendamento.php
@@ -37,7 +37,7 @@ if ($resultadoVerificar->rowCount() > 0) {
 $sql = "INSERT INTO agendamento(cod_usuario, data_atendimento, hora, nome_paciente, sexo, endereco, cpf, endereco_local, cod_profissional,local_atendimento, procedimento, status) VALUES ($cod, '$dia', '$horario', '$paciente', '$sexo', '$endereco', '$cpf', '$endereco_local',$cod_profissional,'$local_atendimento',$procedimento, 0)";
 $resultado = $conexao->prepare($sql);
 if($resultado->execute()){
-    $hoje = date('Y-m-d');
+    $hoje = date('d/m/Y');
     $sql1 = "UPDATE procedimentos SET data_do_agendamento = '$dia', local_do_agendamento = '$local_atendimento', data_da_saida = '$hoje', profissional = '$nome_profissional' WHERE id = $procedimento";
     $resultado1 = $conexao->prepare($sql1);
     $resultado1->execute();
