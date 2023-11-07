9cae6e9413041275d44fb554e8623cc96de53eda
Pedro Yan <134109214+pedroyanalmeida@users.noreply.github.com>
2023-11-07 13:37:44 -0300

    ajustes

--- cad_ag/cadastrar_agenda.php
+++ cad_ag/cadastrar_agenda.php
@@ -20,23 +20,23 @@ $resultado->execute();
 <form method = "POST" action = "registro_agenda.php">  
     <div class="form-outline mb-4">
         <label class="form-label">DIA *</label>
-        <input type="date" name="dia" class="form-control form-control-lg" id="dia" required min="<?= date('Y-m-d'); ?>" required>
+        <input type="date" name="dia" class="form-control form-control-lg" id="dia" required min="<?= date('Y-m-d'); ?>">
     </div>
     <div class="form-outline mb-4">
-        <label class="form-label">Hora Inicio Manha *</label>
-        <input type="time" name="inicio_manha" class="form-control form-control-lg" id="inicio_manha" required>
+        <label class="form-label">Hora Inicio Manha </label>
+        <input type="time" name="inicio_manha" class="form-control form-control-lg" id="inicio_manha">
     </div>
     <div class="form-outline mb-4">
-        <label class="form-label">Hora Final Manha *</label>
-        <input type="time" name="final_manha" class="form-control form-control-lg" id="final_manha" required>
+        <label class="form-label">Hora Final Manha </label>
+        <input type="time" name="final_manha" class="form-control form-control-lg" id="final_manha">
     </div>
     <div class="form-outline mb-4">
-        <label class="form-label">Hora Inicio Tarde *</label>
-        <input type="time" name="inicio_tarde" class="form-control form-control-lg" id="inicio_tarde" required>
+        <label class="form-label">Hora Inicio Tarde </label>
+        <input type="time" name="inicio_tarde" class="form-control form-control-lg" id="inicio_tarde">
     </div>
     <div class="form-outline mb-4">
-        <label class="form-label">Hora Final Tarde *</label>
-        <input type="time" name="final_tarde" class="form-control form-control-lg" id="final_tarde" required>
+        <label class="form-label">Hora Final Tarde </label>
+        <input type="time" name="final_tarde" class="form-control form-control-lg" id="final_tarde">
     </div>
     <input type = "hidden" name = "id" value = "<?= $id; ?>">
     <input type = "hidden" name = "cpf_logado" value = "<?= $cpf_logado; ?>">
